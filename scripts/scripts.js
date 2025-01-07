3808e6303d397315bbbdf022723d7bf7870b3093
budujoseph <bud24002@byui.edu>
2025-01-07 22:31:11 +0000

    still on hamburger button

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,3 +3,11 @@ document.getElementById('currentYear').textContent = currentYear;
 
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modification: ${lastModified}`;
+
+const hamburger = document.querySelector('.top-navs');
+const list = document.querySelector('.top-nav-list');
+
+hamburger.addEventListener('click', () => {
+    list.classList.toggle('show');
+    hamburger.classList.toggle('show');
+})
