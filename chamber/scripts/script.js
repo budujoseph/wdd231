1bf8de662c5fbed0e7202ea1e471b13d4e49928d
budujoseph <bud24002@byui.edu>
2025-01-20 18:21:21 +0000

    lazy loading images

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -57,7 +57,7 @@ const displayMemebrs = (members) => {
 
         img.setAttribute('src', member.imageURL);
         img.setAttribute('alt', `Picture of ${member.name}`);
-        img.setAttribute('loading', 'lazy');
+        // img.setAttribute('loading', 'lazy');
         img.setAttribute('width', '300');
         img.setAttribute('height', '180');
 
