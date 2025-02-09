0ab45bee95bc2cd9d883c273b42c472a5399440d
budujoseph <bud24002@byui.edu>
2025-02-09 20:31:01 +0000

    discover

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -35,5 +35,34 @@ displayPlaces(places);
 
 
 const greetingMsg = document.getElementById("message-content");
-greetingMsg.textContent = "Yo wossop";
+
+const lastVisit = localStorage.getItem('lastVisit');
+console.log(lastVisit);
+
+const currentDate = new Date();
+console.log(currentDate);
+
+let message = "";
+
+if (!lastVisit) {
+    message = "Welcome! Let us know if you have any questions"
+} else {
+    const lastVisitDate = new Date(lastVisit);
+    console.log(lastVisitDate);
+
+    const timeDifference = currentDate - lastVisitDate;
+    console.log(timeDifference);
+
+    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    console.log(daysDifference);
+
+    if (daysDifference < 1) {
+        message = "Back so soon! Awesome!"
+    } else {
+        message = `You last visited ${daysDifference} day${daysDifference === 1 ? "" : "s"} ago`;
+    }
+}
+
+greetingMsg.textContent = message;
+localStorage.setItem("lastVisit", currentDate);
 
